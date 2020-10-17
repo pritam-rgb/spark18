cfbdc30e1963d6c3a93c88cdcdc966e73a617f49
pritam-rgb <pritamchatterjee945@gmail.com>
2020-10-17 12:02:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      country:{
+      Portugal:{
+        Aasiya:false,
+        Luvleen:false,
+        Rey:false,
+        Cayla:false
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      },
+      Nicaragua:{
+        Deveedas:false,
+        Obasey:false,
+        Xenie:false,
+        Ezequiel:false
+      },
+      MarshallIsland:{
+        Aaron:false,
+        Jelena:false
+      }
+    }
+
+    }
+  }
+
+  handleClick=(e)=>{
+    const{name,checked}=e.target;
+    this.setState((prevState)=>{
+      const country=prevState.country;
+      country[name]=checked;
+      return country;
+    });
+  };
+  render(){
+    const favCountry=Object.keys(this.state.country)
+      .filter((key)=> this,state.country[key])
+      .join(",");
+    return(
+      <div className="App">
+        <header className="header">
+          <h1>Handling Multiple checkboxes</h1>
+        </header>
+        <main>
+          <div>
+            <h1>Portugal</h1>
+            <input checked={this.state.country.Portugal.Aasiya} onChange={this.handleClick} type="checkbox" name="Aasiya" />
+            Aasiya Jayavant
+          </div>
+          <div>
+            <input checked={this.state.country.Portugal.Luvleen} onChange={this.handleClick} type="checkbox" name="Luvleen" />
+            Luvleen Lawrence
+          </div>
+          <div>
+            <input checked={this.state.country.Portugal.Aasiya} onChange={this.handleClick} type="checkbox" name="Aasiya" />
+            Aasiya Jayavant
+          </div>
+          <div>
+            <input checked={this.state.country.Portugal.Rey} onChange={this.handleClick} type="checkbox" name="Rey" />
+            Rey Milbourne
+          </div>
+          <div>
+            <input checked={this.state.country.Portugal.Cayla} onChange={this.handleClick} type="checkbox" name="Cayla" />
+            Cayla Brister
+          </div>
+        </main>
+      </div>
+    )
+  }
 }
-
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
